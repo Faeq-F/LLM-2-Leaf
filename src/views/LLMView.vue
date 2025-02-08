907895bda5a745a482ab192d5019bd0ffe9a7b5a
Faeq <faeqfaisal@hotmail.co.uk>
2025-02-08 12:43:08 +0000

    :sparkles: setup page routing

--- src/views/LLMView.vue
+++ src/views/LLMView.vue
@@ -1,14 +1,15 @@
 <script lang="ts">
-import Archive from '@/components/Archive.vue';
+
 </script>
 
 <template>
   <div id="CurrentSite" class="h-screen w-screen p-0 m-0 absolute left-0 top-0">
-    Hello
-    <RouterLink to="/chat">
-      <button class="btn btn-primary">Chat Page</button>
-    </RouterLink>
+    Chat page
+      <RouterLink to="/">
+        <button class="btn btn-primary">Go back</button>
+      </RouterLink>
   </div>
+
 </template>
 
 <style scoped></style>
