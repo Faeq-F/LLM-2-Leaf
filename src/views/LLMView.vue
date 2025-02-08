292dd4d6bb83518381bd4e87c6d341585514f089
Faeq <faeqfaisal@hotmail.co.uk>
2025-02-08 13:03:49 +0000

    :construction: setup preline & fixed old setup details

--- src/views/LLMView.vue
+++ src/views/LLMView.vue
@@ -8,6 +8,8 @@
       <RouterLink to="/">
         <button class="btn btn-primary">Go back</button>
       </RouterLink>
+  <span class="inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-full text-xs font-medium bg-gray-500 text-white">Badge</span>
+
   </div>
 
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    'node_modules/preline/dist/*.js',
+
   ],
-  plugins: [animate, require("rippleui")],
+  plugins: [animate, require("rippleui"), require('preline/plugin'), require('@tailwindcss/forms')],
 };
