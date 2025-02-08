881de67472d5f8f12949590dff83fdd4d010f093
Faeq <faeqfaisal@hotmail.co.uk>
2025-02-08 15:48:18 +0000

    :construction: updated node version for Docker to fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-import Components from 'unplugin-vue-components/vite'
-
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
